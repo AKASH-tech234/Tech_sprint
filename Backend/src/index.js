67d6e188ccde09b1a2a49e63c978d6f0cb7ced16
CodeSangam <prajapativishnumunna@gmail.com>
2025-12-26 16:40:01 +0530

    Add backend source code

---
+++ Backend/src/index.js
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connectDB from "./db/index.js"
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Express initialized 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed ❌", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
